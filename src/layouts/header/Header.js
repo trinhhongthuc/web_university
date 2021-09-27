ab3aa5ec0c297b30b58faffc51d4d63b2acc3d4e
Trinh Hong Thuc <trinhhong.thuc@sapotacorp.vn>
2021-09-27 20:08:01 +0700

    create file header

---
+++ src/layouts/header/Header.js
@@ -0,0 +1,30 @@
+const Header = () => {
+  return (
+    <>
+      <h1 className="header">day la code</h1>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="">home</a>
+            </li>
+            <li>
+              <a href="">about</a>
+            </li>
+            <li>
+              <a href="">about</a>
+            </li>
+            <li>
+              <a href="">about</a>
+            </li>
+            <li>
+              <a href="">about</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
